6e6b46c0bbbbc1459b8921a76f4f5ac98858fee2
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-03 12:58:40 +0000

    Done.

---
+++ lib/chatty_student.rb
@@ -0,0 +1,15 @@
+class ChattyStudent < Student 
+  
+  def raise_hand
+    i=0 
+    while i<10 
+      super 
+      i+=1 
+    end 
+  end 
+  
+  def hello
+    puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+  end 
+  
+end 
